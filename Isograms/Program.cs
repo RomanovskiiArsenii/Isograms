83ea35b30a7aeb697e62be286c493f10d27cd610
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-07-26 21:50:45 +0400

    Добавьте файлы проекта.

---
+++ Isograms/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    /// <summary>
+    /// The method determines whether the string is an isogram.
+    /// </summary>
+    /// <param name="str">input string</param>
+    /// <returns>true if the sting is an isogram</returns>
+    public static bool IsIsogram(string str)
+    {
+        string lowercaseStr = str.ToLower();
+        return lowercaseStr.Distinct().Count() == lowercaseStr.Length;
+    }
+    static void Main()
+    {
+        Console.WriteLine(IsIsogram("isIsogram"));
+    }
+}
